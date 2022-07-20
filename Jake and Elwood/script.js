ce4212648ba247320440580aeca14b38577e5d92
Awosise Oluwaseun <awosiseo@gmail.com>
2022-07-21 00:00:26 +0100

    Jake and Elwood Project completed

---
+++ Jake and Elwood/script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.nav');
+const openButton = document.querySelector('.open-nav');
+const closeButton = document.querySelector('.close-nav');
+
+
+openButton.addEventListener("click", () => {
+    nav.classList.add('navigation-open');
+});
+
+
+closeButton.addEventListener("click", () => {
+    nav.classList.remove('navigation-open');
+});
+
+
