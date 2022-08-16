a25f0cce077a2c0efdf8c586d776dc155ccef259
Awosise Oluwaseun <awosiseo@gmail.com>
2022-08-17 00:07:18 +0100

    mobile navigation

--- manage-landing-page/main.js
+++ manage-landing-page/main.js
@@ -1,23 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const primaryHeader = document.querySelector('.primary-header')
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+navToggle.addEventListener('click', () => {
+  if (primaryNav.hasAttribute("data-visible")) {
+    navToggle.setAttribute("aria-expanded", false)
+  }else {
+    navToggle.setAttribute("aria-expanded", true)
+  }
+  
+  // primaryNav.hasAttribute("data-visible")
+  // ? navToggle.setAttribute("aria-expanded", false)
+  // : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute('data-visible');
+  primaryHeader.toggleAttribute('data-overlay')
+});
